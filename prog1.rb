7d219dfe3aad4410c603be765cabcd124a484ee6
Taylor Carpenter <taylor@codecafe.com>
2009-02-09 00:28:49 -0600

    Let's add some comments for our code.  Comments allow us to write notes for ourselves and other humans to read.  The computer ignores them

--- prog1.rb
+++ prog1.rb
@@ -1,3 +1,6 @@
+# This line is a comment and will be ignored.  Comments start with a #
+# The code below will do nothing
+# puts "What is your name?"
 print "What is your name? "
 name = gets
 name = name.chomp
