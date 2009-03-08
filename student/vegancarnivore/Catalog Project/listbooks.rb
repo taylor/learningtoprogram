59a6226f266dafe2922b219bb8de63583310b6b2
vegancarnivore <devan@gmail.com>
2009-03-08 18:05:21 -0500

    Completed with help from Hal

--- student/vegancarnivore/Catalog Project/listbooks.rb
+++ student/vegancarnivore/Catalog Project/listbooks.rb
@@ -1,2 +1,16 @@
-readlist = File.readlines("booklistfile.txt")
-puts readlist
+list = File.readlines("booklistfile.txt")
+list2 = []
+list.each do |line|
+  title, author = line.chomp.split("|")
+  list2 << [title,author]
+end
+puts list
+
+puts "\n\nSorted by author"
+list2 = list2.sort_by {|x| x[1] }
+list2.each {|book| puts book.join(" by ") }
+
+puts "\n\nSorted by title"
+list2 = list2.sort_by {|x| x[0] }
+list2.each {|book| puts book.join(" by ") }
+
