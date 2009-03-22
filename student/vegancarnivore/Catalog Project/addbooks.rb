74460fb8c1000054a92cbe776cbcf8eca579dc45
Devan Carpenter <devanc@gmail.com>
2009-03-22 17:40:40 -0500

    date for adding book

--- student/vegancarnivore/Catalog Project/addbooks.rb
+++ student/vegancarnivore/Catalog Project/addbooks.rb
@@ -8,8 +8,8 @@ loop do
   puts "Enter author's name: "
   author = gets.chomp
 
-loan_stat = "Not Loaned"
+  loan_stat = "Not Loaned"
   File.open("booklistfile.txt","a") do |f|
-    f.puts "#{title}|#{author}|#{loan_stat}|"
+    f.puts "#{title}|#{author}|#{loan_stat}|#{{Date.today}}"
   end
 end
