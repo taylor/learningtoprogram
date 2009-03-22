754282169537242803f44e8091156d22a5cfb4a5
Devan Carpenter <devan@douglas.home>
2009-03-22 01:30:52 -0500

    Added support for new fields in booklist.txt

--- student/vegancarnivore/Catalog Project/listbooks.rb
+++ student/vegancarnivore/Catalog Project/listbooks.rb
@@ -1,21 +1,26 @@
-list = File.readlines("booklistfile.txt")
+lines = File.readlines("booklistfile.txt")
 
-list2 = []
-list.each do |line|
-  title, author = line.chomp.split("|")
-  list2 << [title,author]
+list = []
+lines.each do |line|
+  title, author,loan_stat,loan_date = line.chomp.split("|")
+  list << [title,author,loan_stat,loan_date]
 end
 
+
 # lines above *could* be written simply as:
-# list2 = list.map {|line| line.chomp.split("|") }
+# list = lines.map {|line| line.chomp.split("|") }
 
-puts list
-
-puts "\n\nSorted by author"
-list2 = list2.sort_by {|x| x[1] }
-list2.each {|book| puts book.join(" by ") }
+puts lines
 
 puts "\n\nSorted by title"
-list2 = list2.sort_by {|x| x[0] }
-list2.each {|book| puts book.join(" by ") }
+list = list.sort_by {|x| x[0] }
+
+list.each do |book|
+  if book[2] == "Not Loaned"
+    s = "#{book[0]} by #{book[1]}"
+  else
+    s = "#{book[0]} by #{book[1]} currently #{book[2]}"
+  end
+  puts s
+end
 
