17de01fe18d4704d1cc964e26ffb42f6a58d98d6
anasanjo <baloo321@hotmail.com>
2009-04-19 14:14:49 -0500

    Added comments, tried different ways of accessing part of the list2 array, added the date.

--- student/anasanjo/catalog_project/listbooks.rb
+++ student/anasanjo/catalog_project/listbooks.rb
@@ -1,23 +1,22 @@
 puts "Hello Mr. Roboto. Welcome to your library application-LIST BOOKS"
  
-listBooks = File.readlines("BookList")
+lines = File.readlines("BookList")
  
-list2 = []
-listBooks.each do |line|
+list = []
+lines.each do |line|
   title, author, loanStatus, loanDate = line.chomp.split("|")
-  list2 << [title,author,loanStatus,loanDate]
+  list << [title,author,loanStatus,loanDate]
 end
  
 # the above 5 lines could also be written:
-# list2 = listBooks.map {|line| line.chomp.split("|") }
+# list = listBooks.map {|line| line.chomp.split("|") }
  
 puts "Unsorted list"
-puts list2
+puts list
  
-list2 = list2.sort_by {|x| x[1]}
-puts "\n\nSorted by author"
-list2.each_with_index {|book,i| puts "Book #{i}: " + book.join(" by ") }
- 
-list2 = list2.sort_by {|x| x[0]}
+list = list.sort_by {|x| x[0]}
 puts "\n\nSorted by title"
-list2.each {|book| puts book.join(" by ") }
+list.each_with_index do |book,i|
+	puts "#{i}: #{book[0]} by #{book[1]} "
+end
+ 
--- student/anasanjo/catalog_project/loanbook.rb
+++ student/anasanjo/catalog_project/loanbook.rb
@@ -1,19 +1,38 @@
 puts "Welcome to the Book Loaning Program."
 
-loop do
- listBooks = File.readlines("BookList")
+books = {}
 
- list2 = listBooks.map {|line| line.chomp.split("|") }
- 
+loop do										#starts a loop
+ listBooks = File.readlines("BookList") #variable listBooks = the lines of BookList
+
+ list2 = listBooks.map {|line| line.chomp.split("|") }	#variable list2 = BookList separated by | and given new lines
+#list2.each_with_index do |book,i|
+#end
+#books[i] = {title, author, loanStatus, loanDate}
+
  puts list2
  
- list2 = list2.sort_by {|x| x[0]}
+ list2 = list2.sort_by {|x| x[0]}						#list2 is now sorted by first elements
 puts "\n\nSorted by title"
-list2.each {|book| puts book.join(" by ") }
+#instead of list2.each {|book| puts book.join(" by ") }
+list2.each_with_index do |book,i|
+ puts "#{i}: #{book[0]} by #{book[1]} "
+end
 
  puts "Choose a book to loan by entering its number. To quit, type 'quit'"
  str = gets.chomp
  
  break if str == "quit"
  choice = str
+ 
+ #bookSelected = books[choice.to_i[0]]
+ #puts bookSelected
+ 
+ #list2.each do |book|
+  #puts "#{choice}: #{book[0]} by #{book[1]}"
+ #end
+ 
+  # using choice, select loanStatus element, or element 2 of that array row, and changed to Loaned.
+  # how can i do this... ?
+  
  end
